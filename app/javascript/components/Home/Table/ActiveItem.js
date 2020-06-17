cdd3567fc24edc4468f29859ef918923d4b65dbb
Luzaks <kanemekanik@gmail.com>
2020-06-17 13:52:10 -0500

    Feat: Add temporary layout for ActiveItem component.

---
+++ app/javascript/components/Home/Table/ActiveItem.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import Thumbnail from './Thumbnail';
+import styled from 'styled-components';
+
+const Button = styled.a`
+    display: inline-block;
+    text-decoration: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radious: 0;
+    background: #fff;
+    padding: 00px 20px;
+    font-size: 18px;
+    color: #333 !important;
+    box-shadow: 0px 0px 0px 3px #473228,
+    -6px 6px #ef5f17,
+    -6px 6px 0px 3px #473228;
+`;
+
+const ActiveItem = (props) => {
+
+    return(
+        <div className="row pt-4 pb-4">
+            <div className="col-md-10 offset-md-1">
+                <div className="text-center">
+                    <div className="card px-5">
+                        <div className="row">
+                            <div className="col-md-10 ossfet-md-1">
+                                <Thumbnail />
+                                <div className="pt-4 pb-4">
+                                    <h4>{ props.title }</h4>
+                                    <p>{props.description}</p>
+                                    <div className="cta-wrapper">
+                                        <Button className="btn cta-btn">Watch this video</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ActiveItem;
