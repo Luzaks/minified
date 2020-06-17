b5c3370c973ae844327f19730f6d17e54bb7b424
Luzaks <kanemekanik@gmail.com>
2020-06-17 13:47:10 -0500

    Style: Add style for Items components displyed on the screen.

--- app/javascript/components/Home/Table/Item.js
+++ app/javascript/components/Home/Table/Item.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
-import Thumbnail from './Thumbnail'
+import Thumbnail from './Thumbnail';
+import styled from 'styled-components';
+
+const Button = styled.a`
+    display: inline-block;
+    text-decoration: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radious: 0;
+    background: #fff;
+    padding: 00px 20px;
+    font-size: 18px;
+    color: #333 !important;
+    box-shadow: 0px 0px 0px 3px #473228,
+    -6px 6px #ef5f17,
+    -6px 6px 0px 3px #473228;
+`;
 
 const Item = (props) => {
 
     return(
-        <div className="row">
+        <div className="row pt-4 pb-4">
             <div className="col-md-10 offset-md-1">
                 <div className="text-center">
                     <div className="card px-5">
@@ -17,7 +33,7 @@ const Item = (props) => {
                                     <h4>{ props.title }</h4>
                                     <p>{props.description}</p>
                                     <div className="cta-wrapper">
-                                        <a className="btn cta-btn">Watch this video</a>
+                                        <Button className="btn cta-btn">Watch this video</Button>
                                     </div>
                                 </div>
                             </div>
